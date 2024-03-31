a87aee5835da26311f70c0e82628dba8266e7ad7
NightRaven <erickdavid77@hotmail.es>
2024-03-30 20:40:22 -0600

    Avance de la pokedex del día 30 de marzo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flet as ft
+import asyncio
 
 
 async def main(page: ft.Page):
@@ -8,30 +9,41 @@ async def main(page: ft.Page):
     page.padding = 0
 
     botonAzul = ft.Stack(
-        [ft.Container(width=80, height=80, bgcolor=ft.colors.WHITE,
-                      border=ft.border.all(), border_radius=50),
+        [ft.Container(width=80, height=80, bgcolor=ft.colors.WHITE, border_radius=50),
          ft.Container(width=70, height=70, left=5, top=5, bgcolor=ft.colors.BLUE, border_radius=50)]
     )
 
     itemsSuperior = [
-        ft.Container(botonAzul, width=80, height=80, border=ft.border.all()),
-        ft.Container(width=40, height=40, border=ft.border.all()),
-        ft.Container(width=40, height=40, border=ft.border.all()),
-        ft.Container(width=40, height=40, border=ft.border.all()),
+        ft.Container(botonAzul, width=80, height=80),
+        ft.Container(width=40, height=40,
+                     bgcolor=ft.colors.RED_200, border_radius=50),
+        ft.Container(width=40, height=40,
+                     bgcolor=ft.colors.YELLOW, border_radius=50),
+        ft.Container(width=40, height=40,
+                     bgcolor=ft.colors.GREEN, border_radius=50),
     ]
+
+    itemCentral = ft.Stack([
+        ft.Container(width=600, height=400, bgcolor=ft.colors.WHITE),
+        ft.Container(width=550, height=350,
+                     bgcolor=ft.colors.BLACK, top=25, left=25),
+        ft.Image(
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/197.png", scale=10, width=50, height=50, top=350 / 2, right=550/2
+        )
+    ])
+
+    itemInferior = [ft.Container(width=50, border=ft.border.all()),]
 
     superior = ft.Container(
         content=ft.Row(itemsSuperior),
         width=600,
         height=80,
         margin=ft.margin.only(top=40),
-        border=ft.border.all(),
     )
-    centro = ft.Container(
-        width=600, height=400, margin=ft.margin.only(top=40), border=ft.border.all()
-    )
+    centro = ft.Container(content=itemCentral, width=600, height=400,
+                          margin=ft.margin.only(top=40), alignment=ft.alignment.center)
     inferior = ft.Container(
-        width=600, height=400, margin=ft.margin.only(top=40), border=ft.border.all()
+        content=ft.Row(itemInferior), width=600, height=400, margin=ft.margin.only(top=40), border=ft.border.all()
     )
 
     col = ft.Column(spacing=0, controls=[superior, centro, inferior])
